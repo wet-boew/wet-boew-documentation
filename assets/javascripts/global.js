5126b32b6a20e86aed5fe5782f5a1a646b695201
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2013-08-22 12:03:43 -0400

    Added WET license URLs to CORS check.

--- assets/javascripts/global.js
+++ assets/javascripts/global.js
@@ -1,12 +1,17 @@
-/*global jQuery: false, ActiveXObject: false*/
-(function ($) {
+/*!
+ *
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.io/wet-boew/License-eng.html / wet-boew.github.io/wet-boew/Licence-fra.html
+ *
+ */
+/*global jQuery: false*/
+(function () {
 	"use strict";
 	// Test that browser supports CORS (uses test from prose/boot.js). Intended to reroute to fallback when Prose won't work. 
 	if (!('withCredentials' in new XMLHttpRequest())) {
-		var $editLink = $('#edit-page'),
-			request = false;
-		if ($editLink.length !== 0) {
-			$editLink.attr('href', $editLink.attr('href').replace('http://prose.io/#ogpl/ogpl.github.io/edit/', 'https://github.com/ogpl/ogpl.github.io/blob/'));
+		var edit_link = document.getElementById('edit-page');
+		if (edit_link !== null) {
+			edit_link.href = edit_link.href.replace(/^http:\/\/prose\.io\/#/, 'https://github.com/').replace(/\/edit\//, '/blob/');
 		}
 	}
 }(jQuery));
