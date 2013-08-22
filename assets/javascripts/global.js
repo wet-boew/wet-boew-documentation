d1864f9723d8c9e0adcc7a6290952d2954b299fe
Paul Jackson <Paul.Jackson@tbs-sct.gc.ca>
2013-08-22 11:53:26 -0400

    Fixed the CORS check and fallback.

--- assets/javascripts/global.js
+++ assets/javascripts/global.js
@@ -1,12 +1,11 @@
-/*global jQuery: false, ActiveXObject: false*/
-(function ($) {
+/*global jQuery: false*/
+(function () {
 	"use strict";
 	// Test that browser supports CORS (uses test from prose/boot.js). Intended to reroute to fallback when Prose won't work. 
 	if (!('withCredentials' in new XMLHttpRequest())) {
-		var $editLink = $('#edit-page'),
-			request = false;
-		if ($editLink.length !== 0) {
-			$editLink.attr('href', $editLink.attr('href').replace('http://prose.io/#ogpl/ogpl.github.io/edit/', 'https://github.com/ogpl/ogpl.github.io/blob/'));
+		var edit_link = document.getElementById('edit-page');
+		if (edit_link !== null) {
+			edit_link.href = edit_link.href.replace(/^http:\/\/prose\.io\/#/, 'https://github.com/').replace(/\/edit\//, '/blob/');
 		}
 	}
 }(jQuery));
